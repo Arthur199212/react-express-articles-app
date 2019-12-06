bcc7030b2da174e37f47529c3a523fe236ecff5e
Artur Rubchenko <Artur_Rubchenko@epam.com>
2019-12-06 16:07:02 +0300

    refactor: rename folders

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-
-const v1 = require('./src/routes/api')
+const api = require('./app/routes/api')
 
 const app = express()
 
@@ -22,10 +21,7 @@ mongoose.connect(MONGO_URI, {
   .then(() => console.log('\x1b[36m%s\x1b[0m', 'DB Connected!'))
   .catch(err => console.log('\x1b[31m%s\x1b[0m', `DB Connection Error: ${err.message}`))
 
-// Middleware
-app.use('/public', express.static('static'))
-
 // Routes
-app.use('/v1', v1)
+app.use('/v1', api)
 
 app.listen(port, () => console.log('\x1b[1m%s\x1b[0m', `Listening on http://localhost:${port}/`))
